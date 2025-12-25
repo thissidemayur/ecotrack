918a8bede76826118f03a0fd1b8b8da0578ce95e
thissidemayur <thissidemayur@gmail.com>
2025-12-25 11:05:05 +0530

    backend:add shalow healthchecker bussinness logic for aws to does not block our application

--- backend/src/routes/healthCheck.js
+++ backend/src/routes/healthCheck.js
@@ -1,36 +1,49 @@
 import  { Router } from "express";
 import mongoose from "mongoose";
 import { redisClient } from "../config/redis.js";
+// const healthCheckRouter = Router();
+
+// healthCheckRouter.get ("/health", async(req, res) => {
+
+
+//     try {
+//         const dbStatus = mongoose.connection.readyState === 1;
+//         const redisStatus = (await redisClient.ping()) === "PONG";
+
+//         if (!dbStatus || !redisStatus) {
+//           return res.status(503).json({
+//             status: "unhealthy",
+//             checks: {
+//               mongodb: dbStatus ? "up" : "down",
+//               redis: redisStatus ? "up" : "down",
+//             },
+//           });
+//         }
+
+//         res.status(200).json({
+//           status: "healthy",
+//           checks: {
+//             mongodb: "up",
+//             redis: "up",
+//           },
+//         });
+//     } catch (error) {
+//         console.error("Health check failed:", err);
+//         res.status(503).send("Unhealthy");
+//     }
+ 
+// })
+
+
 const healthCheckRouter = Router();
 
-healthCheckRouter.get ("/health", async(req, res) => {
-
-    try {
-        const dbStatus = mongoose.connection.readyState === 1;
-        const redisStatus = (await redisClient.ping()) === "PONG";
-
-        if (!dbStatus || !redisStatus) {
-          return res.status(503).json({
-            status: "unhealthy",
-            checks: {
-              mongodb: dbStatus ? "up" : "down",
-              redis: redisStatus ? "up" : "down",
-            },
-          });
-        }
-
-        res.status(200).json({
-          status: "healthy",
-          checks: {
-            mongodb: "up",
-            redis: "up",
-          },
-        });
-    } catch (error) {
-        console.error("Health check failed:", err);
-        res.status(503).send("Unhealthy");
-    }
- 
-})
+/**
+ * @description Shallow health check for AWS ALB.
+ * Responds with 200 OK instantly to prove the Node.js process is alive.
+ */
+healthCheckRouter.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 export { healthCheckRouter };
+// export { healthCheckRouter };
