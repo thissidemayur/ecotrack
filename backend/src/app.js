af4e5cf98e6cd0719e44a8e8e03f1673eac588be
thissidemayur <thissidemayur@gmail.com>
2026-05-20 13:16:30 +0530

    refactor: write down healthcheckRouter just after parser,cors middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,10 @@ import { securityHeaders } from "./middlewares/security.middleware.js"
 // imports for middlewares and routes
 import { healthCheckRouter } from "./routes/healthCheck.js"
 import router from "./routes/index.js"
+
 const app = express()
 
-app.use(healthCheckRouter);
 
-// 1. Security Middlewares (Helmet)
-// add several criticals HTTP headers to protect aganist common attacks
 app.use(helmet())
 
 app.use(
@@ -25,27 +23,17 @@ app.use(
   })
 );
 
-// Global Security: Apply to every single request hitting the server
 app.use(securityHeaders)
 
-// 2. Rate Limiting (General API Protection) - DDoS and Brute-Force Protection
-app.use(globalRateLimiter)
-
-// 3. CORS configuration
-
-// 4. Request body parser
 app.use(express.json({limit:"16kb"})) // parse incoming JSON payloads(upto 16kb to prevent DoS attacks)
 app.use(express.urlencoded({extended:true, limit:"16kb"})) // parse URL-encoded payloads (often used with HTML forms)
 app.use(cookieParser()) // parse cookies from incoming request headers
 
-// 5. Sanitization Middlewares
-// app.use(sanitizedBodyMiddleware);
-// 5. Centeral Controllers (all api routes)
+app.use(globalRateLimiter)
+
 app.use("/api/v1",router)
+app.use(healthCheckRouter);
 
 // 6. Error Handling Middlewares
-// app.use(notFound) // middleware to catch rqst to undefined routes (404 errors)
-app.use(errorHandler) //Global error handler middleware (handle errros parse via next(err) and our ApiError class defined in utils/ApiError.js) )
-
-
+app.use(errorHandler)
 export default app
