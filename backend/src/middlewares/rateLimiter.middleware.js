ed96d0dfed6a2469fc5ce6a554878c48a8caa404
thissidemayur <thissidemayur@gmail.com>
2026-05-20 13:15:15 +0530

    feat: add json response while using expressratelimiter

--- backend/src/middlewares/rateLimiter.middleware.js
+++ backend/src/middlewares/rateLimiter.middleware.js
@@ -4,34 +4,35 @@ import { ApiError } from '../utils/apiError.utils.js'
 
 // apply to all API routes
 const globalRateLimiter = rateLimit({
-    limit: config.RATE_LIMIT_MAX,
-    legacyHeaders:false, // disable the `X-RateLimit-*` headers
-    standardHeaders:"draft-8", // enable the `RateLimit-*` headers
-    keyGenerator:(req,res,next)=>{
-        return ipKeyGenerator(req.ip)
-    },
-    handler:(req,res,next,options)=>{
-        const error = new ApiError(options.statusCode,"Too many requests , please try again later.",[])
-        next(error)
-    }
-})
+  limit: config.RATE_LIMIT_MAX,
+  legacyHeaders: false, // disable the `X-RateLimit-*` headers
+  standardHeaders: "draft-8", // enable the `RateLimit-*` headers
+  keyGenerator: (req, res, next) => {
+    return ipKeyGenerator(req.ip);
+  },
+  handler: (req, res, next, options) => {
+    return res.status(options.statusCode).json({
+      success: false,
+      message: `Too many login attempts. Please try again in ${Math.ceil(
+        options.windowMs / 1000,
+      )} seconds.`,
+    });
+  },
+});
 
 
 // stricter rate limiter for auth related routes (login,signup,forgot password)
 const authRateLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 5 minutes
-  limit: 2, // max 20 requests per windowMs
+  windowMs: 1 * 60 * 1000,
+  limit: 20,
   standardHeaders: true,
   legacyHeaders: false,
+
   handler: (req, res, next, options) => {
-    const error = new ApiError(
-      options.statusCode,
-      `Too many login attempts. Please try again in ${Math.ceil(
-        options.windowMs / 1000
-      )} seconds.`,
-      []
-    );
-    next(error);
+    return res.status(options.statusCode).json({
+      success: false,
+      message: `Too many login attempts. Please try again later.`,
+    });
   },
 });
 
